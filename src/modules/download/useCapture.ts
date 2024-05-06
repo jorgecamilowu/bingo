bdd79bb76d071e270e5693581025d82ff5fd8f42
jorgewu <jorge.wu@optilyz.com>
2024-05-06 07:37:38 +0200

    implement download feature

---
+++ src/modules/download/useCapture.ts
@@ -0,0 +1,27 @@
+import { RefObject } from "react";
+import html2canvas from "html2canvas";
+
+export function useCapture(ref: RefObject<HTMLDivElement>) {
+  async function capture() {
+    if (ref.current === null) {
+      return;
+    }
+
+    const canvas = await html2canvas(ref.current);
+
+    const dataURL = canvas.toDataURL("[bingo_card]/png");
+
+    const anchor = document.createElement("a");
+    anchor.target = "_blank";
+    anchor.href = dataURL;
+    anchor.download = dataURL;
+
+    document.body.appendChild(anchor);
+
+    anchor.click();
+
+    document.body.removeChild(anchor);
+  }
+
+  return capture;
+}
